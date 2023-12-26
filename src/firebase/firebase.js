03a1a9e17117fb3e100d94834098d806bc960894
amaan35 <amaanlm35@gmail.com>
2023-12-26 10:25:27 +0530

    Creating a project named "movie-site" on firebase, adding configuration file named firebase.js, installing firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC9fByMZfUqWQG14PsMFIUaCDAqMRuv6HA",
+  authDomain: "movie-site-4d94d.firebaseapp.com",
+  projectId: "movie-site-4d94d",
+  storageBucket: "movie-site-4d94d.appspot.com",
+  messagingSenderId: "109613003942",
+  appId: "1:109613003942:web:dde693b61f1c9afb21b067"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
