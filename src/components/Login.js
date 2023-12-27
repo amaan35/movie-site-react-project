7acb84fb3ce870e809fc47f34819b6b67ce8017a
amaan35 <amaanlm35@gmail.com>
2023-12-27 21:25:17 +0530

    Styling Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
+import { TailSpin } from "react-loader-spinner";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+    const [form, setForm] = useState({
+        mobile: "",
+        password: ""
+    });
+    const [loading, setLoading] = useState(false);
   return (
-    <div>
-      
+    <div className="w-full flex flex-col items-center mt-8">
+      <h1 className="text-xl font-bold">Login</h1>
+      <div class="p-2 md:w-1/3 w-full">
+        <div class="relative">
+          <label for="message" class="leading-7 text-sm text-gray-300">
+            Mobile Number
+          </label>
+          <input
+            type={"number"}
+            id="message"
+            name="message"
+            value={form.mobile}
+            onChange={(e) => setForm({ ...form, mobile: e.target.value })}
+            class="w-full bg-gray-300 rounded border border-gray-300 focus:border-indigo-500 focus:bg-gray-200 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          ></input>
+        </div>
+      </div>
+      <div class="p-2 md:w-1/3 w-full">
+        <div class="relative">
+          <label for="message" class="leading-7 text-sm text-gray-300">
+            Password
+          </label>
+          <input
+            id="message"
+            name="message"
+            value={form.password}
+            onChange={(e) => setForm({ ...form, password: e.target.value })}
+            class="w-full bg-gray-300 rounded border border-gray-300 focus:border-indigo-500 focus:bg-gray-200 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          ></input>
+        </div>
+      </div>
+      <div class="p-2 w-full">
+        <button
+          class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg"
+        >
+          {loading ? <TailSpin height={25} color="white" /> : "Login"}
+        </button>
+      </div>
+      <div>
+        <p>Do not have an account? <Link to={'/signup'}><span className="text-blue-500">Sign up</span></Link></p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
