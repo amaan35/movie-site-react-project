3aa4705800c7ea39add2b558381c91cc510a9db2
amaan35 <amaanlm35@gmail.com>
2023-12-27 21:01:16 +0530

    Routing Login and Signup component in App component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Routes, Route } from "react-router-dom";
 import AddMovie from "./components/AddMovie";
 import Detail from "./components/Detail";
 import { createContext, useState } from "react";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 const AppState = createContext();
 
@@ -20,6 +22,8 @@ function App() {
           <Route path="/" element={<Cards />} />
           <Route path="/addmovie" element={<AddMovie />} />
           <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </AppState.Provider>
