5b17dae44774826bde937d3b15ce578d13e310e1
amaan35 <amaanlm35@gmail.com>
2023-12-27 20:47:08 +0530

    Creating context in App component and wrapping our app in AppState

--- src/App.js
+++ src/App.js
@@ -4,18 +4,27 @@ import Cards from "./components/Cards";
 import { Routes, Route } from "react-router-dom";
 import AddMovie from "./components/AddMovie";
 import Detail from "./components/Detail";
+import { createContext, useState } from "react";
+
+const AppState = createContext();
 
 function App() {
+  const [login, setLogin] = useState(false);
+  const [userName, setUserName] = useState("");
+
   return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Cards />} />
-        <Route path="/addmovie" element={<AddMovie />} />
-        <Route path="/detail/:id" element={<Detail />} />
-      </Routes>
-    </div>
+    <AppState.Provider value={{login, userName, setLogin, setUserName}}>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Cards />} />
+          <Route path="/addmovie" element={<AddMovie />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+      </div>
+    </AppState.Provider>
   );
 }
 
 export default App;
+export {AppState};
