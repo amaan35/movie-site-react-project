06d965f4d03b8473150d051353c15940ff42561e
amaan35 <amaanlm35@gmail.com>
2023-12-28 21:17:02 +0530

    Creating a separate file to handle responsiveness of the website, showing movie cards in grid instead of flexbox, changing the grid size as the screen size changes, adding few more movies

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,6 +4,7 @@ import { Grid } from "react-loader-spinner";
 import ReactStars from "react-stars";
 import { moviesRef } from "../firebase/firebase";
 import { Link } from "react-router-dom";
+import '../styles/Responsive.css';
 
 const Cards = () => {
   const [data, setData] = useState([]);
@@ -12,7 +13,7 @@ const Cards = () => {
   useEffect(() => {
     async function getData() {
       setLoading(true);
-      setData([]);
+      // setData([]);
       const _data = await getDocs(moviesRef);
       _data.forEach((doc)=>{
         setData((prv)=>[...prv, {...(doc.data()), id: doc.id}])
@@ -22,17 +23,17 @@ const Cards = () => {
     getData();
   }, []);
   return (
-    <div className="flex flex-wrap justify-between p-3 mt-2">
+    <div className="movie-grid p-3 mt-2 w-full">
       {loading ? (
-        <div className="w-full flex justify-center items-center min-h-screen"><Grid height={40} color="white" /></div>
+        <div className="flex justify-center items-center h-screen w-screen"><Grid height={40} color="white" /></div>
       ) : (
         data.map((e, i) => {
           return (
             <Link to={`/detail/${e.id}`}><div
               key={i}
-              className="md:p-2 p-1 bg-black font-medium rounded-lg shadow-xl hover:-translate-y-3 cursor-pointer mb-5 transition-all duration-400"
+              className="md:p-2 p-1 bg-black font-medium rounded-lg shadow-xl hover:-translate-y-3 cursor-pointer mb-5 transition-all duration-400 md:ml-2"
             >
-              <img className="md:h-96 h-60" src={e.image} />
+              <img className="md:h-96 w-full object-cover object-top" src={e.image} />
               <h1>
                 {e.title}
               </h1>
